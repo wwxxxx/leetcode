127bdb95d8cfef17f9edfc77d32755ca0ce52b2d
wwxxxx <875249815@qq.com>
2018-04-07 22:22:06 +0800

    commit

---
+++ 21. Merge Two Sorted Lists/21.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+     int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *p, *tmp;
+        p = new ListNode(0);
+        tmp = p;
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                tmp->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                tmp->next = l2;
+                l2 = l2->next;
+            }
+            tmp = tmp->next;
+        }
+        if (l1)
+            tmp->next = l1;
+        if (l2)
+            tmp->next = l2;
+        return p->next;
+    }
+};
