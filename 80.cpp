c4dc36f209efeee354438f61e9be96fc2e6cb34f
wwxxxx <875249815@qq.com>
2018-08-09 01:55:48 +0800

    commit

---
+++ 80.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+static int x = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution
+{
+  public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        int size = nums.size(), cnt = 0;
+        for (int i = 2; i < size; ++i)
+        {
+            if (nums[i] == nums[i - 2 - cnt])
+                ++cnt;
+            else
+                nums[i - cnt] = nums[i];
+        }
+        return size - cnt;
+    }
+};
